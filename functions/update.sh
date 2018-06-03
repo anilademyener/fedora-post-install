bea9c448ae6bf3bf55c0fa83012d7a4530605bfb
Louis <louisweddall@gmail.com>
2018-06-02 19:36:27 -0500

    Delete update.sh

--- functions/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo dnf clean all	# Cleans package cache
-sudo dnf update -y	# Updates system and accepts all prompts
-sudo dnf autoremove -y	# Removes packages no longer needed
-flatpak update		# Updates Flatpak programs
