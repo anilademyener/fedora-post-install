446af9a4f87a0370308fe59c9fc5c6891aa646a0
Louis <louisweddall@gmail.com>
2018-06-02 19:58:11 -0500

    Fix permissions

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ sudo cp postinstall /usr/bin
 sudo mkdir /opt/postinstall
 sudo cp main.py /opt/postinstall
 sudo cp -r functions /opt/postinstall
+chmod a+x uninstall.sh
+sudo chmod a+x /opt/postinstall/main.py
+sudo chmod a+x /opt/postinstall/functions/*
